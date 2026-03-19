4860c8093be9592dfaf3fb53bf8c2a3f76220e5a
Robert Marks <robjmarks@gmail.com>
2026-03-19 14:16:56 +0000

    uk joint filing check validation

--- ui/sidebar.py
+++ ui/sidebar.py
@@ -13,7 +13,7 @@ def render_sidebar() -> JobOptions:
 
     disclosure_system = st.sidebar.selectbox(
         "Disclosure system",
-        options=["none", "efm", "esef"],
+        options=["none", "hmrc", "esef"],
         index=0,
     )
 
